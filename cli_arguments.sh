62f4574a042696b0f65a13690d813acfe779266b
AdamouGarba-web <adamougarbar@gmail.com>
2025-05-20 18:55:30 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
-# Vérifie s’il y a au moins un argument
+# Fonction pour afficher les arguments ligne par ligne
+print_arguments() {
+  local index=1
+  for arg in "$@"; do
+    echo "Argument $index: $arg"
+    ((index++))
+  done
+}
+# Vérifie la présence d'au moins un argument
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 arg1 [arg2 ...]"
+  echo "Usage : $0 <arg1> <arg2> ..."
   exit 1
 fi
-
-# Compteur de position
-i=1
-
-# Parcourt tous les arguments
-for arg in "$@"; do
-  echo "Argument $i: $arg"
-  i=$((i + 1))
-done
+print_arguments "$@"
